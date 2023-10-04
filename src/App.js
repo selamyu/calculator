8d89272ec422fde0d58e248a81aa213f040e5f1d
Tesfahiwet <selamyu2021@gmail.com>
2023-10-04 10:22:50 -0500

    Add files via upload
    developed a simple calculator using React JavaScript.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MainBody from './component/MainBody';
+
+function App() {
+  return (
+    <div className="App">
+      <MainBody />
+    </div>
+  );
+}
+
+
+export default App;
